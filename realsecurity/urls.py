dfd77c0432353332abd5130b1c7883cd7683e475
Tarun Garg <45597781+tarungarg1999@users.noreply.github.com>
2020-08-31 14:57:02 +0530

    Update urls.py

--- realsecurity/urls.py
+++ realsecurity/urls.py
@@ -4,5 +4,6 @@ urlpatterns = [
     path('', views.home,name='homepage'),
     path('home.html', views.home,name='homepage'),
     path('Services.html', views.Services,name='Services'),
-    path('Contact.html', views.Contact,name='Contact')
+    path('Contact.html', views.Contact,name='Contact'),
+    path('pawn_check.html', views.pawn_check,name='Pawn Checking')
 ]
