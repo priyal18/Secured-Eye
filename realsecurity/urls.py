5db7aa25d63c296dad5cd2baf8a5b7f8bc6a86f3
root <root@localhost.localdomain>
2020-08-31 14:21:27 +0530

    First commit

---
+++ realsecurity/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('', views.home,name='homepage'),
+    path('home.html', views.home,name='homepage'),
+    path('Services.html', views.Services,name='Services'),
+    path('Contact.html', views.Contact,name='Contact')
+]
